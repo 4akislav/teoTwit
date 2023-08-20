e0e5710b084dfddb0c5d07f4c6f95c54c010eaa6
Vlad <127046214+4akislav@users.noreply.github.com>
2023-08-20 18:21:10 +0300

    trying to repair prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"
 import { SignIn } from "@clerk/nextjs";
+import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 
-export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+const Home: NextPage = () => {
 
   const user = useUser();
+
+  const {data} = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -21,8 +23,14 @@ export default function Home() {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"/>
+        <div>
+          {data?.map((post) => (
+          <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
 }
+
+export default Home;
